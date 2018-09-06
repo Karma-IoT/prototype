4c844382bd5dc42038336a91a2fba6ed41cddd88
tiannian <dtiannian@aliyun.com>
2018-09-07 00:18:39 +0800

    change keystore

--- keystore.py
+++ keystore.py
@@ -1,51 +1,60 @@
 import nacl
 import nacl.signing
 import nacl.encoding
-import json
-import os.path
 import hashlib
 
 class Keystore:
-    def __init__(self,path = '',diffcult = 5):
-        if path == '':
-            self.path = './keystore.json'
-            data = {}
-            while 1:
-                sk = nacl.signing.SigningKey.generate()
-                data['sk'] = sk.encode(encoder = nacl.encoding.HexEncoder).decode()
-                data['pk'] = sk.verify_key.encode(encoder = nacl.encoding.HexEncoder).decode()
-                addr = hashlib.blake2s(sk.verify_key.encode(),digest_size = 20)
-                p = hashlib.blake2s(addr.digest(),digest_size = 20)
-                data['addr'] = addr.hexdigest()
-                data['diffcult'] = diffcult
-                #  print('{:0160b}'.format(int.from_bytes(p.digest(),byteorder = 'big')))
-                if '{:0160b}'.format(int.from_bytes(p.digest(),byteorder = 'big'))[:diffcult] == ('{:0'+ str(diffcult) +'b}').format(0):
-                    break
-            self.data = data
-
-        else:
-            self.path = path
-            self.data = json.load(open(path,'r'))
-            
-    def store(self):
-        json.dump(self.data,open(self.path,'w+'))
-
+    def __init__(self,diffcult = 5):
+        while 1:
+            sk = nacl.signing.SigningKey.generate()
+            self.sk = sk.encode(encoder = nacl.encoding.HexEncoder).decode()
+            self.pk = sk.verify_key.encode(encoder = nacl.encoding.HexEncoder).decode()
+            addr = hashlib.blake2s(sk.verify_key.encode(),digest_size = 20)
+            p = hashlib.blake2s(addr.digest(),digest_size = 20)
+            self.addr = addr.hexdigest()
+            self.diffcult = diffcult
+            #  print('{:0160b}'.format(int.from_bytes(p.digest(),byteorder = 'big')))
+            if '{:0160b}'.format(int.from_bytes(p.digest(),byteorder = 'big'))[:diffcult] == ('{:0'+ str(diffcult) +'b}').format(0):
+                break
     def sign(self,data):
-        sk = nacl.signing.SigningKey(self.data['sk'],encoder = nacl.encoding.HexEncoder)
+        sk = nacl.signing.SigningKey(self.sk,encoder = nacl.encoding.HexEncoder)
         sm = sk.sign(data)
         return sm.signature,sm.message
 
     def verify(self,data,signature):
-        pk = nacl.signing.VerifyKey(self.data['pk'],encoder = nacl.encoding.HexEncoder)
+        pk = nacl.signing.VerifyKey(self.pk,encoder = nacl.encoding.HexEncoder)
         try:
             return pk.verify(data,signature = signature)
         except nacl.exceptions.BadSignatureError as e:
             print(e)
             return False
 
+def KeystoreJSONEncode(obj):
+    if isinstance(obj,Keystore):
+        s = {}
+        s['sk'] = obj.sk
+        s['pk'] = obj.pk
+        s['addr'] = obj.addr
+        s['diffcult'] = obj.diffcult
+        s['_class'] = 'Keystore'
+        return s
+    else:
+        return obj
+
+def KeystoreJSONDecode(o):
+    if o['_class'] == 'Keystore':
+        keystore = Keystore()
+        keystore.sk = o['sk']
+        keystore.pk = o['pk']
+        keystore.addr = o['addr']
+        keystore.diffcult = o['diffcult']
+        return router
+    else:
+        return o
 
 
+            
 if __name__ == '__main__':
+    import json
     k1 = Keystore()
-    print(k1.data)
-    k1.store()
+    print(json.dumps(k1,indent = 4, default = KeystoreJSONEncode))
