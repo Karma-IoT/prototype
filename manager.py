d5bda6615f4e0e90c488e2ac150559716d50852e
tiannian <dtiannian@aliyun.com>
2018-09-07 14:32:01 +0800

    add manager for serde

---
+++ manager.py
@@ -0,0 +1,27 @@
+import os.path
+import json
+
+class Manager:
+    def __init__(self,path,cls,*args):
+        self.path = path
+        if os.path.exists(path):
+            # Load data
+            self.ins = json.load(open(path,'r'),object_hook = cls.JSONDecode)
+        else:
+            # Create new
+            self.ins = cls(*args)
+            json.dump(self.ins,open(path,'w+'),indent = 4,default = cls.JSONEncode)
+        
+    def flush(self):
+        json.dump(self.ins,open(path,'w+'),indent = 4,default = cls.JSONEncode)
+
+
+if __name__ == '__main__':
+    import keystore
+    manager = Manager('keystore.json',keystore.Keystore)
+    print(type(manager.ins))
+
+    import router
+    mrouter = Manager('router.json',router.Router)
+    print(mrouter.ins)
+
